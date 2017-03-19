42ec9fa21218c2f8598258c84f325601ede3610d
tonib <toni.bennasar@gmail.com>
2017-03-19 21:25:50 +0100

    - Moved code to new folder "src" (Preparation for build scripts)

--- prepareBuild.sh
+++ prepareBuild.sh
@@ -1,13 +1,13 @@
 # Script to prepare a build to upload Project Aon
 
 # Clean and rebuild Android apk
+cd src
 rm www/kai.apk
 cordova clean android
 cordova build android
 
-# Copy the apk to do the final test on local machine and to upload to Project Aon
-cp platforms/android/build/outputs/apk/android-debug.apk /var/www/kai.apk
+# Copy the apk to upload to Project Aon
 cp platforms/android/build/outputs/apk/android-debug.apk www/kai.apk
 
 # Download this on you test Android device to test the apk:
-# http://[LOCALPCIP]/kai.apk
+# http://[LOCALPCIP]/[KAICHRONICLESDIR]/kai.apk
