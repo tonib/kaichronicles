601cbc4dd37a6c7bbfa4391d805a2a4678a6511f
tonib <toni.bennasar@gmail.com>
2017-08-05 07:59:43 +0200

    - SectionRenderer to TS

--- src/ts/external.ts
+++ src/ts/external.ts
@@ -4,7 +4,6 @@ declare var gameView:any;
 declare var template:any;
 declare var cordovaApp:any;
 declare var projectAon:any;
-declare var SectionRenderer:any;
 declare var views:any;
 declare var routing:any;
 declare var gameController:any;
