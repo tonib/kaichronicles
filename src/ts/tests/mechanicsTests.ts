8664ff959972cbddfd47acc981e2f0eddb558162
tonib <toni.bennasar@gmail.com>
2017-09-19 19:29:13 +0200

    - Testing XSD to validate the mechanics files

---
+++ src/ts/tests/mechanicsTests.ts
@@ -0,0 +1,43 @@
+
+/**
+ * Tools to validate book mechanics
+ */
+
+import libxml = require( 'libxmljs' );
+import fs = require('node-fs-extra');
+declare var process;
+
+console.log("Mechanics tests...");
+declare var __dirname;
+console.log( process.cwd() );
+
+
+const xsd : string = fs.readFileSync( 'src/ts/tests/mechanics.xsd' , 'utf8' );
+const xsdDoc = libxml.parseXml(xsd);
+
+const xmlText : string = fs.readFileSync( 'src/ts/tests/test_file.xml' , 'utf8' );
+const xmlDoc = libxml.parseXml(xmlText);
+if( xmlDoc.validate(xsdDoc) )
+    console.log('XML OK!');
+else {
+    console.log('Do\'h!');
+    console.log( xmlDoc.validationErrors );
+}
+
+/*
+var xsd = '<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"><xs:element name="comment"><xs:complexType><xs:all><xs:element name="author" type="xs:string"/><xs:element name="content" type="xs:string"/></xs:all></xs:complexType></xs:element></xs:schema>';
+var xml_valid = '<?xml version="1.0"?><comment><author>author</author><content>nothing</content></comment>';
+var xml_invalid = '<?xml version="1.0"?><comment>A comment</comment>';
+
+var xsdDoc = libxml.parseXml(xsd);
+var xmlDocValid = libxml.parseXml(xml_valid);
+var xmlDocInvalid = libxml.parseXml(xml_invalid);
+
+console.log(xmlDocValid.validate(xsdDoc), true);
+console.log(xmlDocValid.validationErrors.length, 0);
+console.log(xmlDocInvalid.validate(xsdDoc), false);
+
+console.log(xmlDocInvalid.validationErrors.length, 2);
+console.log(xmlDocInvalid.validationErrors);
+*/
+
