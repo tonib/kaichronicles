f55b30aebd43b3fdf0f4ab086990c07ff7a488c5
tonib <toni.bennasar@gmail.com>
2017-09-19 21:39:37 +0200

    - More work on XSD

--- src/ts/tests/mechanicsTests.ts
+++ src/ts/tests/mechanicsTests.ts
@@ -27,23 +27,6 @@ try {
     }
 }
 catch(e) {
-    console.log(e);
+    console.log( 'Error validating: ' + e.toString() );
 }
-
-/*
-var xsd = '<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"><xs:element name="comment"><xs:complexType><xs:all><xs:element name="author" type="xs:string"/><xs:element name="content" type="xs:string"/></xs:all></xs:complexType></xs:element></xs:schema>';
-var xml_valid = '<?xml version="1.0"?><comment><author>author</author><content>nothing</content></comment>';
-var xml_invalid = '<?xml version="1.0"?><comment>A comment</comment>';
-
-var xsdDoc = libxml.parseXml(xsd);
-var xmlDocValid = libxml.parseXml(xml_valid);
-var xmlDocInvalid = libxml.parseXml(xml_invalid);
-
-console.log(xmlDocValid.validate(xsdDoc), true);
-console.log(xmlDocValid.validationErrors.length, 0);
-console.log(xmlDocInvalid.validate(xsdDoc), false);
-
-console.log(xmlDocInvalid.validationErrors.length, 2);
-console.log(xmlDocInvalid.validationErrors);
-*/
 
