e960d710f0f9319deb09070524ff029be0713f4f
tonib <toni.bennasar@gmail.com>
2018-03-24 12:14:43 +0100

    - FAQ (not finished)

--- src/www/js/views/mainMenuView.js
+++ src/www/js/views/mainMenuView.js
@@ -28,6 +28,10 @@ var mainMenuView = {
             e.preventDefault();
             mainMenuController.changeTranslation();
         });
+        $('#menu-faq').click(function(e) {
+            e.preventDefault();
+            routing.redirect('faq');
+        });
 
         // Download books (only for app)
         var $downloadBooksBtn = $('#menu-downloadbooks');
