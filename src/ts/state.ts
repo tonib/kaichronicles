de660e6725339cd12cd8f4b7e2b23162a809581e
cracrayol <cracrayol@free.fr>
2019-11-07 01:14:25 +0100

    TSLint of root files

--- src/ts/state.ts
+++ src/ts/state.ts
@@ -1,90 +1,91 @@
-/// <reference path="external.ts" />
-
-/** 
+/**
  * The application state
-*/
+ */
 const state = {
 
     /**
      * The current book
      */
-    book : <Book> null,
+    book : null as Book,
 
     /**
      * The current book mechanics
      */
-    mechanics: <Mechanics> null,
+    mechanics: null as Mechanics,
 
     /**
      * The current book section states
      */
-    sectionStates: <BookSectionStates> null,
+    sectionStates: null as BookSectionStates,
 
     /**
      * The current action chart
      */
-    actionChart: <ActionChart> null,
+    actionChart: null as ActionChart,
 
     /**
      * The current language ('en' = english / 'es' = spanish)
      */
-    language: 'en',
+    language: "en",
 
     /**
      * Color Theme ( 'light' or 'dark' ).
      * This is stored at localStorage['color'], not with the game state
      */
-    color: 'light',
+    color: "light",
 
     /**
      * The local books download state for the Cordova app.
      * This member is not persisted
      */
-    localBooksLibrary: <LocalBooksLibrary> null,
+    localBooksLibrary: null as LocalBooksLibrary,
 
     /**
      * Setup the default browser language
      */
-    setupDefaultLanguage: function() {
-        console.log('Current language: ' + navigator.language);
-        if( !navigator.language || navigator.language.length < 2 )
+    setupDefaultLanguage() {
+        // console.log("Current language: " + navigator.language);
+        if ( !navigator.language || navigator.language.length < 2 ) {
             return;
-        if( navigator.language.toLowerCase().substr(0,2) == 'es' )
-            state.language = 'es';
+        }
+        if ( navigator.language.toLowerCase().substr(0, 2) === "es" ) {
+            state.language = "es";
+        }
     },
 
     /**
      * Setup the default color or persist from local storage
      */
-    setupDefaultColorTheme: function() {
-        
+    setupDefaultColorTheme() {
+
         try {
-            state.color = localStorage.getItem( 'color' );
-            if( !state.color )
-                state.color = 'light';
-        }
-        catch(e) {
-            state.color = 'light';
-            console.log(e);
+            state.color = localStorage.getItem( "color" );
+            if ( !state.color ) {
+                state.color = "light";
+            }
+        } catch (e) {
+            state.color = "light";
+            // console.log(e);
         }
     },
 
     /**
      * Setup the state for a book number and language
      */
-    setup: function(bookNumber : number, language : string, keepActionChart : boolean) {
+    setup(bookNumber: number, language: string, keepActionChart: boolean) {
 
-        if( !bookNumber )
+        if ( !bookNumber ) {
             bookNumber = 1;
+        }
 
         state.sectionStates = new BookSectionStates();
 
         // Action chart
         state.actionChart = null;
-        if( keepActionChart ) {
+        if ( keepActionChart ) {
             // Try to get the previous book action chart, and set it as the current
             state.actionChart = state.getPreviousBookActionChart(bookNumber - 1);
-            if( state.actionChart ) {
+            if ( state.actionChart ) {
                 // Convert the Object to ActionChart
                 state.actionChart = $.extend(new ActionChart(), state.actionChart);
             }
@@ -97,11 +98,12 @@ const state = {
         state.book = new Book(bookNumber, state.language);
         state.mechanics = new Mechanics(state.book);
 
-        if( !state.actionChart )
+        if ( !state.actionChart ) {
             state.actionChart = new ActionChart();
+        }
     },
 
-    removeCachedState: function() {
+    removeCachedState() {
         state.book = null;
         state.mechanics = null;
         state.sectionStates = null;
@@ -111,88 +113,92 @@ const state = {
     /**
      * Reset the current state
      */
-    reset: function(deleteBooksHistory : boolean) {
+    reset(deleteBooksHistory: boolean) {
 
         state.removeCachedState();
 
         // Remove current game state
-        localStorage.removeItem( 'state' );
+        localStorage.removeItem( "state" );
 
-        if( deleteBooksHistory ) {
+        if ( deleteBooksHistory ) {
             // Remove action charts from previous books
-            for(var i=1; i <= projectAon.getLastSupportedBook(); i++)
-                localStorage.removeItem( 'state-book-' + i.toString() );
+            for (let i = 1; i <= projectAon.getLastSupportedBook(); i++) {
+                localStorage.removeItem( "state-book-" + i.toString() );
+            }
         }
     },
 
     /**
      * Returns the current state object
      */
-    getCurrentState: function() {
+    getCurrentState() {
         return {
-            bookNumber: state.book ? state.book.bookNumber : 0,
             actionChart: state.actionChart,
+            bookNumber: state.book ? state.book.bookNumber : 0,
+            language: state.language,
             sectionStates: state.sectionStates,
-            language: state.language
         };
     },
 
     /**
      * Store the current state at the browser local storage
      */
-    persistState: function() {
+    persistState() {
         try {
-            var json = JSON.stringify( state.getCurrentState() );
-            localStorage.setItem( 'state' , json );
-        }
-        catch(e) {
-            console.log(e);
+            const json = JSON.stringify( state.getCurrentState() );
+            localStorage.setItem( "state" , json );
+        } catch (e) {
+            // console.log(e);
+            throw new Error(e);
         }
     },
 
     /**
      * Return true if there is an stored persisted state
      */
-    existsPersistedState: function() {
-        return localStorage.getItem( 'state' );
+    existsPersistedState() {
+        return localStorage.getItem( "state" );
     },
 
     /**
      * Restore the state from the local storage
      */
-    restoreState: function() {
+    restoreState() {
         try {
-            var json = localStorage.getItem( 'state' );
-            if( !json )
-                throw "No state to restore found";
-            var stateKeys = JSON.parse( json );
-            if( !stateKeys )
-                throw 'Wrong JSON format';
+            const json = localStorage.getItem( "state" );
+            if ( !json ) {
+                throw new Error("No state to restore found");
+            }
+            const stateKeys = JSON.parse( json );
+            if ( !stateKeys ) {
+                throw new Error("Wrong JSON format");
+            }
             state.restoreStateFromObject( stateKeys );
-        }
-        catch(e) {
-            console.log(e);
-            state.setup(1, 'en', false);
+        } catch (e) {
+            // console.log(e);
+            state.setup(1, "en", false);
         }
     },
 
     /**
      * Restore the state from an object
      */
-    restoreStateFromObject: function(stateKeys) {
+    restoreStateFromObject(stateKeys) {
 
         // On version 1.6.3 / 1.7, the stateKeys.actionChart.weaponSkill has been changed from string to Array<string> (magnakai)
-        if( typeof stateKeys.actionChart.weaponSkill === 'string' ) {
-            if( stateKeys.actionChart.weaponSkill )
+        if ( typeof stateKeys.actionChart.weaponSkill === "string" ) {
+            if ( stateKeys.actionChart.weaponSkill ) {
                 stateKeys.actionChart.weaponSkill = [ stateKeys.actionChart.weaponSkill ];
-            else
+            } else {
                 stateKeys.actionChart.weaponSkill = [];
+            }
         }
 
         // On version 1.6.3 / 1.7, we store the number of arrows (magnakai)
-        if( !stateKeys.actionChart.arrows )
+        if ( !stateKeys.actionChart.arrows ) {
             stateKeys.actionChart.arrows = 0;
-        
+        }
+
         state.language = stateKeys.language;
         state.book = new Book(stateKeys.bookNumber, state.language);
         state.mechanics = new Mechanics(state.book);
@@ -204,7 +210,7 @@ const state = {
     /**
      * Update state to change the book language
      */
-    updateBookTranslation: function(book) {
+    updateBookTranslation(book) {
         state.book = book;
         state.mechanics.book = book;
         state.language = book.language;
@@ -214,15 +220,15 @@ const state = {
      * Update state to change the book language
      * @param color 'light' or 'dark'
      */
-    updateColorTheme: function(color) {
+    updateColorTheme(color) {
         state.color = color;
-        localStorage.setItem( 'color' , state.color );
+        localStorage.setItem( "color" , state.color );
     },
 
     /**
      * Restore objects on the Kai Monastery section from the Action Chart
      */
-    restoreKaiMonasterySectionObjects : function() {
+    restoreKaiMonasterySectionObjects() {
         const kaiMonasterySection = state.sectionStates.getSectionState( Book.KAIMONASTERY_SECTION );
         kaiMonasterySection.objects = state.actionChart ? state.actionChart.kaiMonasterySafekeeping : [];
     },
@@ -230,10 +236,10 @@ const state = {
     /**
      * Update state to start the next book
      */
-    nextBook: function() {
+    nextBook() {
 
         // Save the action chart state on the current book ending
-        var key = 'state-book-' + state.book.bookNumber.toString();
+        const key = "state-book-" + state.book.bookNumber.toString();
         localStorage.setItem( key , JSON.stringify( state.actionChart ) );
 
         // Move to the next book
@@ -252,31 +258,33 @@ const state = {
      * @param {number} bookNumber Book which get the action chart
      * @returns {object} The action chart. null if it was not found. The returned value is an Object, not an ActionChart
      */
-    getPreviousBookActionChart: function(bookNumber) : any {
-        var key = 'state-book-' + bookNumber.toString();
-        var json = localStorage.getItem( key );
-        if( !json )
+    getPreviousBookActionChart(bookNumber): any {
+        const key = "state-book-" + bookNumber.toString();
+        const json = localStorage.getItem( key );
+        if ( !json ) {
             return null;
-        return JSON.parse(json); 
+        }
+        return JSON.parse(json);
     },
 
     /**
      * Returns the object to save the game state
      */
-    getSaveGameJson: function() {
+    getSaveGameJson() {
 
         // Get the current state
-        var saveGameObject = {
+        const saveGameObject = {
             currentState: state.getCurrentState(),
-            previousBooksState: {}
+            previousBooksState: {},
         };
 
         // Get the action charts at the end of each book
-        for(var i=1; i<= 30; i++) {
-            var key = 'state-book-' + i;
-            var previousBookState = localStorage.getItem( key );
-            if( previousBookState )
+        for (let i = 1; i <= 30; i++) {
+            const key = "state-book-" + i;
+            const previousBookState = localStorage.getItem( key );
+            if ( previousBookState ) {
                 saveGameObject.previousBooksState[i] = previousBookState;
+            }
         }
         return JSON.stringify( saveGameObject );
     },
@@ -284,30 +292,31 @@ const state = {
     /**
      * Restore the game from a save game file
      */
-    loadSaveGameJson: function(json) {
+    loadSaveGameJson(json) {
 
-        // replace BOM Character (https://en.wikipedia.org/wiki/Byte_order_mark). Otherwise call to JSON.parse will fail 
-        json = json.replace(/\ufeff/g, '');
+        // replace BOM Character (https://en.wikipedia.org/wiki/Byte_order_mark). Otherwise call to JSON.parse will fail
+        json = json.replace(/\ufeff/g, "");
 
-        //alert( json );
-        //console.log( json );
-        var saveGameObject = JSON.parse( json );
-        
+        // alert( json );
+        // console.log( json );
+        const saveGameObject = JSON.parse( json );
+
         // Restore current state
-        if( !saveGameObject || !saveGameObject.currentState)
-            throw "Wrong format";
+        if ( !saveGameObject || !saveGameObject.currentState) {
+            throw new Error("Wrong format");
+        }
         state.restoreStateFromObject(saveGameObject.currentState);
 
         // Restore previous books action chart
-        for(var i=1; i<= 30; i++) {
-            var key = 'state-book-' + i;
-            if( saveGameObject.previousBooksState[i] ) {
+        for (let i = 1; i <= 30; i++) {
+            const key = "state-book-" + i;
+            if ( saveGameObject.previousBooksState[i] ) {
                 localStorage.setItem( key , saveGameObject.previousBooksState[i] );
-            }
-            else
+            } else {
                 localStorage.removeItem( key );
+            }
         }
 
         state.persistState();
-    }
+    },
 };
--- src/ts/template.ts
+++ src/ts/template.ts
@@ -1,5 +1,3 @@
-/// <reference path="external.ts" />
-
 /**
  * The HTML template API
  */
@@ -12,122 +10,122 @@ const template = {
      * @param showTitleOnSmallDevs True if the main title should be shown on
      * small devices.
      */
-    setNavTitle: function(title : string, url : string, showTitleOnSmallDevs : boolean ) { 
+    setNavTitle(title: string, url: string, showTitleOnSmallDevs: boolean ) {
         // Update the title
-        $('#template-title').text(title);
-        $('#template-title').attr('href', url);
-        $('#template-img-logo').attr('href', url);
+        $("#template-title").text(title);
+        $("#template-title").attr("href", url);
+        $("#template-img-logo").attr("href", url);
 
-        var $title = $('#template-title');
-        if( showTitleOnSmallDevs )
-            $title.removeClass('hidden-xs hidden-sm');
-        else
-            $title.addClass('hidden-xs hidden-sm');
-            
+        const $title = $("#template-title");
+        if ( showTitleOnSmallDevs ) {
+            $title.removeClass("hidden-xs hidden-sm");
+        } else {
+            $title.addClass("hidden-xs hidden-sm");
+        }
+
     },
 
     /**
-    * Hightlight the active navigation bar link
-    */
-    highlightActiveLink: function() {
-        $('#template-header a, #template-header li').removeClass('active');
-        var $actives = $('#template-header a[href="#' + 
+     * Hightlight the active navigation bar link
+     */
+    highlightActiveLink() {
+        $("#template-header a, #template-header li").removeClass("active");
+        const $actives = $('#template-header a[href="#' +
             routing.normalizeHash(location.hash) + '"]');
-        $actives.each(function(index, link) {
-            var $link = $(link);
+        $actives.each((index, link) => {
+            const $link = $(link);
             // Bootstrap puts the class 'active' on the parent of the link
             // But I want to remark the "brand" link too, so put it on both
-            $link.parent().filter('li').addClass('active');
-            $link.addClass('active');
+            $link.parent().filter("li").addClass("active");
+            $link.addClass("active");
         });
     },
 
     /**
      * Setup navigation bar
      */
-    setup: function() {
+    setup() {
 
         // Hide the bootstrap menu when some menu option is clicked, or when
         // the content is clicked
-        $('#template-header a, #template-container').click(function(){
+        $("#template-header a, #template-container").click(() => {
             template.collapseMenu();
         });
-        $('#template-statistics').click(function() {
-            routing.redirect('actionChart');
+        $("#template-statistics").click(() => {
+            routing.redirect("actionChart");
         });
         template.updateStatistics(true);
         template.translateMainMenu();
         template.changeColorTheme(state.color);
 
-        if( cordovaApp.isRunningApp() ) {
+        if ( cordovaApp.isRunningApp() ) {
             // If we are on the cordova app, disable the animation (performance)
-            $('#navbar').addClass('no-transition');
+            $("#navbar").addClass("no-transition");
 
             // Link not needed: You have the "back" hardware button
-            $('#template-mainMenu').hide();
+            $("#template-mainMenu").hide();
         }
     },
 
     /**
-    * Show / hide statistics on navigation bar
-    */
-    showStatistics: function(show : boolean) {
-        if( show ) {
-            $('#navbar-content').show();
-            $('#template-menubutton').removeClass( 'hideImportant' );
+     * Show / hide statistics on navigation bar
+     */
+    showStatistics(show: boolean) {
+        if ( show ) {
+            $("#navbar-content").show();
+            $("#template-menubutton").removeClass( "hideImportant" );
             template.updateStatistics();
-        }
-        else {
-            $('#navbar-content').hide();
-            $('#template-statistics').hide();
-            $('#template-menubutton').addClass( 'hideImportant' );
+        } else {
+            $("#navbar-content").hide();
+            $("#template-statistics").hide();
+            $("#template-menubutton").addClass( "hideImportant" );
         }
     },
 
     /**
      * Update player statistics
      */
-    updateStatistics: function(doNotAnimate : boolean = false) {
+    updateStatistics(doNotAnimate: boolean = false) {
 
         // Update statistics
-        if( !state.actionChart || 
+        if ( !state.actionChart ||
             ( state.actionChart.combatSkill === 0 && state.actionChart.endurance === 0 ) ) {
-            $('#template-statistics').hide();
-            $('#template-map').hide();
-        }
-        else {
-            $('#template-statistics').show();
-            $('#template-combatSkill').text( state.actionChart.getCurrentCombatSkill() );
-            template.animateValueChange( $('#template-endurance') , 
-            state.actionChart.currentEndurance , doNotAnimate , 
-            state.actionChart.currentEndurance > 0 ? null : 'red' );
+            $("#template-statistics").hide();
+            $("#template-map").hide();
+        } else {
+            $("#template-statistics").show();
+            $("#template-combatSkill").text( state.actionChart.getCurrentCombatSkill() );
+            template.animateValueChange( $("#template-endurance") ,
+            state.actionChart.currentEndurance , doNotAnimate ,
+            state.actionChart.currentEndurance > 0 ? null : "red" );
 
             // Update map link
-            if( state.actionChart.hasObject('map') )
-                $('#template-map').show();
-            else
-                $('#template-map').hide(); 
+            if ( state.actionChart.hasObject("map") ) {
+                $("#template-map").show();
+            } else {
+                $("#template-map").hide();
+            }
         }
     },
 
     /**
      * Return true if the template menu is expanded
      */
-    isMenuExpanded: function() : boolean { 
-        return $('#template-menubutton').attr('aria-expanded') == 'true'; 
+    isMenuExpanded(): boolean {
+        return $("#template-menubutton").attr("aria-expanded") === "true";
     },
 
     /**
      * Collapse the template menu
      */
-    collapseMenu: function() { $('#navbar').collapse('hide'); },
+    collapseMenu() { $("#navbar").collapse("hide"); },
 
     /**
      * Show an HTML view
      * @param {DOM} viewContent The view to show
      */
-    setViewContent: function(viewContent : any) {
-        $('#body').html(viewContent);
+    setViewContent(viewContent: any) {
+        $("#body").html(viewContent);
         // Scroll to top
         window.scrollTo(0, 0);
         template.highlightActiveLink();
@@ -136,36 +134,37 @@ const template = {
     /**
      * Display an error
      */
-    setErrorMessage: function(msg : string) {
-        template.setViewContent('<p style="color: red">' + msg + '</p>');
+    setErrorMessage(msg: string) {
+        template.setViewContent('<p style="color: red">' + msg + "</p>");
     },
-    
+
     /**
      * Show dialog with object details
      * @param o The object to show
      */
-    showObjectDetails: function(o : Item) {
-        if( !o )
+    showObjectDetails(o: Item) {
+        if ( !o ) {
             return;
-
-        // Translate the dialog
-        translations.translateTags( $('#template-objectDetails') );
-
-        $('#template-objectTitle').text( o.name );
-
-        // Show / hide object image
-        var imageUrl = o.getImageUrl();
-        if( !imageUrl )
-            $('#template-objectImage').hide();
-        else {
-            $('#template-objectImage').show();
-            $('#template-objectImage img').attr('src' , imageUrl);
         }
 
-        $('#template-objectDescription').text(o.description);
-        $('#template-objectDescriptionExtra').text(o.extraDescription);
-        
-        $('#template-objectDetails').modal('show');
+        // Translate the dialog
+        translations.translateTags( $("#template-objectDetails") );
+
+        $("#template-objectTitle").text( o.name );
+
+        // Show / hide object image
+        const imageUrl = o.getImageUrl();
+        if ( !imageUrl ) {
+            $("#template-objectImage").hide();
+        } else {
+            $("#template-objectImage").show();
+            $("#template-objectImage img").attr("src" , imageUrl);
+        }
+
+        $("#template-objectDescription").text(o.description);
+        $("#template-objectDescriptionExtra").text(o.extraDescription);
+
+        $("#template-objectDetails").modal("show");
     },
 
     /**
@@ -177,91 +176,92 @@ const template = {
      * @param newColor The final HTML color of the element. If it's null, the default
      * color for the DOM element will be used
      */
-    animateValueChange: function( $element : any , newValue : number , doNotAnimate : boolean , newColor : string = null ) {
+    animateValueChange( $element: any , newValue: number , doNotAnimate: boolean , newColor: string = null ) {
 
         // Disable animations on Cordova app (bad performance)
-        if( cordovaApp.isRunningApp() )
+        if ( cordovaApp.isRunningApp() ) {
             doNotAnimate = true;
-        else
+        } else {
             // Clear previous animations
             $element.stop(true, true);
+        }
 
         // If the value is not going to change, do nothing
-        var txtNewValue = newValue.toString();
-        if( $element.text() == txtNewValue )
+        const txtNewValue = newValue.toString();
+        if ( $element.text() === txtNewValue ) {
             return;
-
-        if( doNotAnimate ) {
-            $element.text( txtNewValue );
-            $element.css('color', newColor ? newColor : '' );
         }
-        else {
-            var miliseconds = 500;
-            var currentValue = parseInt( $element.text() );
-            $element.css('color', newValue < currentValue ? 'red' : 'green' );
+
+        if ( doNotAnimate ) {
+            $element.text( txtNewValue );
+            $element.css("color", newColor ? newColor : "" );
+        } else {
+            const miliseconds = 500;
+            const currentValue = parseInt( $element.text(), 10 );
+            $element.css("color", newValue < currentValue ? "red" : "green" );
             $element.fadeOut(miliseconds, function() {
-                $(this).css('color', newColor ? newColor : '');
+                $(this).css("color", newColor ? newColor : "");
                 $(this).text( txtNewValue ).fadeIn(miliseconds);
             });
         }
     },
 
-    translateMainMenu: function() {
-        translations.translateTags( $('#template-header') );
+    translateMainMenu() {
+        translations.translateTags( $("#template-header") );
     },
 
     /**
      * Show the dialog with the combat tables
      */
-    showCombatTables: function() {
+    showCombatTables() {
         // Translate the dialog
-        translations.translateTags( $('#template-combatTables') );
-        
+        translations.translateTags( $("#template-combatTables") );
+
         // Hide toasts
         toastr.clear();
 
         // Set the translated images
-        var combatTablesUrls = state.book.getCombatTablesImagesUrls(state.mechanics);
-        $('#template-ctimage0').attr('src', combatTablesUrls[0]);
-        $('#template-ctimage1').attr('src', combatTablesUrls[1]);
-        $('#template-combatTables').modal('show');
+        const combatTablesUrls = state.book.getCombatTablesImagesUrls(state.mechanics);
+        $("#template-ctimage0").attr("src", combatTablesUrls[0]);
+        $("#template-ctimage1").attr("src", combatTablesUrls[1]);
+        $("#template-combatTables").modal("show");
     },
 
     /**
      * Show / hide the random table dialog
      */
-    showRandomTable: function(show : boolean) {
-        var $randomModal = $('#template-randomtable');
-        if( show ) {
+    showRandomTable(show: boolean) {
+        const $randomModal = $("#template-randomtable");
+        if ( show ) {
             // Hide toasts
             toastr.clear();
             // Translate the dialog
             translations.translateTags( $randomModal );
         }
-        $randomModal.modal( show ? 'show' : 'hide' );
+        $randomModal.modal( show ? "show" : "hide" );
     },
 
     /**
      * Populate the random table values with the current book random table
      */
-    fillRandomTableModal: function(numbers : Array<number>) {
+    fillRandomTableModal(numbers: number[]) {
 
         // Fill table
-        var html = '';
-        for(var row=0; row<10; row++) {
-            html += '<tr>';
-            for(var column=0; column<10; column++) {
-                var number = numbers[ row * 10 + column ];
-                html += '<td data-number="' + number + '">' + number + '</td>';
+        let html = "";
+        for (let row = 0; row < 10; row++) {
+            html += "<tr>";
+            for (let column = 0; column < 10; column++) {
+                const num = numbers[ row * 10 + column ];
+                html += '<td data-number="' + num + '">' + num + "</td>";
             }
-            html += '</tr>';
+            html += "</tr>";
         }
-        $('#template-randomcontent').html( html );
+        $("#template-randomcontent").html( html );
 
         // Add click event handlers:
-        $('#template-randomcontent td').mousedown(function(e) {
+        $("#template-randomcontent td").mousedown(function(e) {
             e.preventDefault();
-            randomTable.randomTableUIClicked( parseInt( $(this).attr('data-number') ) );
+            randomTable.randomTableUIClicked( parseInt( $(this).attr("data-number"), 10 ) );
         });
     },
 
@@ -269,17 +269,17 @@ const template = {
      * Change the color theme of the templates
      * @param theme 'light' or 'dark'
      */
-    changeColorTheme: function(theme: string) {
+    changeColorTheme(theme: string) {
         state.updateColorTheme( theme );
-        
-        switch(theme) {
-            case 'dark':
-                $('body').addClass('dark');
+
+        switch (theme) {
+            case "dark":
+                $("body").addClass("dark");
                 break;
             default:
                 // we will default to "light" theme, or no class
-                $('body').removeClass('dark');
+                $("body").removeClass("dark");
                 break;
         }
-    }
+    },
 };
