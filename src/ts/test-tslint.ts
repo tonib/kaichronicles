c2d64145c445afcf2426a61f014ed674f5a3e24e
tonib <toni.bennasar@gmail.com>
2020-01-26 21:38:37 +0100

    Test Typescript arrow functions with es3 generation (seems to work)

--- src/ts/test-tslint.ts
+++ src/ts/test-tslint.ts
@@ -1,4 +1,5 @@
 
+/*
 function test(xx: number) {
     if (xx == 10) {
         console.log("Is ten");
@@ -12,3 +13,26 @@ const r = 100;
 // x = ( r < 1000 ? '10' : 10 );
 x = "10";
 test(x);
+*/
+
+// Test arrow functions
+class Test {
+
+    public x = 0;
+
+    public increment() {
+        this.x++;
+    }
+
+    public incrementWithArrow() {
+        const f = () => { this.x++; };
+        f();
+    }
+
+}
+
+const x = new Test();
+x.increment();
+console.log(x.x);
+x.incrementWithArrow();
+console.log(x.x);
