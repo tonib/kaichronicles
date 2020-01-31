9153f89cac68b144db6dbe34ed1c78b8429bffaa
tonib <toni.bennasar@gmail.com>
2020-01-31 18:06:29 +0100

    Fix src/ts/controller/mechanics/skillsSetup.ts spaces

--- src/ts/controller/mechanics/skillsSetup.ts
+++ src/ts/controller/mechanics/skillsSetup.ts
@@ -11,51 +11,51 @@ class SkillsSetup {
     public static setSkills() {
 
         // Add mechanics info, only for first played book
-        if ( state.getPreviousBookActionChart( state.book.bookNumber - 1) == null) {
-            gameView.appendToSection( mechanicsEngine.getMechanicsUI("mechanics-actionChartInfo"), "afterTitle" );
+        if (state.getPreviousBookActionChart(state.book.bookNumber - 1) == null) {
+            gameView.appendToSection(mechanicsEngine.getMechanicsUI("mechanics-actionChartInfo"), "afterTitle");
         }
 
         // If the skills are already set, do nothing
-        if ( state.actionChart.combatSkill !== 0 && state.actionChart.endurance !== 0 ) {
+        if (state.actionChart.combatSkill !== 0 && state.actionChart.endurance !== 0) {
             return;
         }
 
         // Add HTML to do the choose
-        gameView.appendToSection( mechanicsEngine.getMechanicsUI("mechanics-setSkills") );
+        gameView.appendToSection(mechanicsEngine.getMechanicsUI("mechanics-setSkills"));
 
         // Disable next link
         gameView.enableNextLink(false);
 
         // Book language inconsistencies: Spanish books say "ignore zero", english does not.
         // Starting from book 13 (Grand Master), Spanish books allow zero:
-        const ignoreZero = ( state.book.language === "es" && state.book.bookNumber < 13 );
+        const ignoreZero = (state.book.language === "es" && state.book.bookNumber < 13);
 
         // Combat skill
-        if ( state.actionChart.combatSkill !== 0 ) {
+        if (state.actionChart.combatSkill !== 0) {
             $("#mechanics-detWeapon").hide();
         } else {
             const $w = $("#mechanics-chooseWeapon");
-            randomMechanics.bindTableRandomLink( $w , function(value) {
+            randomMechanics.bindTableRandomLink($w , function(value) {
                 state.actionChart.combatSkill = value + (state.book.isGrandMasterBook() ? 25 : 10);
-                $w.parent().append( "<b>" + translations.text("combatSkillSet" , [state.actionChart.combatSkill] ) + ".</b>" );
+                $w.parent().append("<b>" + translations.text("combatSkillSet" , [state.actionChart.combatSkill]) + ".</b>");
                 template.updateStatistics();
-                if ( state.actionChart.combatSkill !== 0 && state.actionChart.endurance !== 0 ) {
+                if (state.actionChart.combatSkill !== 0 && state.actionChart.endurance !== 0) {
                     gameView.enableNextLink(true);
                 }
             }, ignoreZero, false);
         }
 
         // Endurance points
-        if ( state.actionChart.endurance !== 0 ) {
+        if (state.actionChart.endurance !== 0) {
             $("#mechanics-detEndurance").hide();
         } else {
             const $e = $("#mechanics-chooseEndurance");
-            randomMechanics.bindTableRandomLink( $e , function(value) {
+            randomMechanics.bindTableRandomLink($e , function(value) {
                 state.actionChart.endurance = value + (state.book.isGrandMasterBook() ? 30 : 20);
                 state.actionChart.currentEndurance = state.actionChart.endurance;
-                $e.parent().append("<b>" + translations.text("enduranceSet" , [state.actionChart.endurance]) + ".</b>" );
+                $e.parent().append("<b>" + translations.text("enduranceSet" , [state.actionChart.endurance]) + ".</b>");
                 template.updateStatistics();
-                if ( state.actionChart.combatSkill !== 0 && state.actionChart.endurance !== 0 ) {
+                if (state.actionChart.combatSkill !== 0 && state.actionChart.endurance !== 0) {
                     gameView.enableNextLink(true);
                 }
             }, ignoreZero, false);
