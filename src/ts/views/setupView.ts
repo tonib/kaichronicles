892d97d3c505f5b979df87c4afe50a729fc7c5a3
tonib <toni.bennasar@gmail.com>
2020-02-15 23:00:13 +0100

    Merge src/ts/views/setupView.ts

--- src/ts/views/setupView.ts
+++ src/ts/views/setupView.ts
@@ -1,16 +1,14 @@
-/// <reference path="../external.ts" />
-
 /**
  * The setup view API
  */
 const setupView = {
     log(text: string, level: string = null) {
         let style: string = "";
-        if ( level == "ok" ) {
+        if (level === "ok") {
             style = 'style="color: green"';
-        } else if ( level == "error" ) {
+        } else if (level === "error") {
             style = 'style="color: red; font-weight: bold"';
- }
+        }
         $("#setup-log").append("<div " + style + ">" + text + "</div>");
     },
 
