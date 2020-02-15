641e838b27ed68b83613386dc1ce5a81f84c1a7f
tonib <toni.bennasar@gmail.com>
2020-02-15 22:56:56 +0100

    Fix src/ts/views/settingsView.ts

--- src/ts/views/settingsView.ts
+++ src/ts/views/settingsView.ts
@@ -8,33 +8,33 @@ const settingsView = {
     setup() {
 
         // Language
-        $("#settings-language").val( state.language );
+        $("#settings-language").val(state.language);
         $("#settings-language").change(function() {
-            settingsController.changeLanguage( $(this).val() , true );
+            settingsController.changeLanguage($(this).val() , true);
         });
 
         // Color theme
-        $("#settings-color-theme").val( state.color );
+        $("#settings-color-theme").val(state.color);
         $("#settings-color-theme").change(function() {
             settingsController.changeColorTheme($(this).val());
         });
 
         // Random table type
-        $("#settings-randomtable").val( state.actionChart.manualRandomTable ? "manual" : "computer" );
+        $("#settings-randomtable").val(state.actionChart.manualRandomTable ? "manual" : "computer");
         $("#settings-randomtable").change(function() {
-            state.actionChart.manualRandomTable = ( $(this).val() == "manual" );
+            state.actionChart.manualRandomTable = ($(this).val() == "manual");
         });
 
         // Extended CRT
-        $("#settings-extendedcrt").val( state.actionChart.extendedCRT ? "yes" : "no" );
+        $("#settings-extendedcrt").val(state.actionChart.extendedCRT ? "yes" : "no");
         $("#settings-extendedcrt").change(function() {
-            state.actionChart.extendedCRT = ( $(this).val() == "yes" );
+            state.actionChart.extendedCRT = ($(this).val() == "yes");
         });
 
         // Restart book
         $("#settings-restart").click(function(e) {
             e.preventDefault();
-            if ( confirm( translations.text("confirmRestart") ) ) {
+            if (confirm(translations.text("confirmRestart"))) {
                 setupController.restartBook();
             }
         });
@@ -56,22 +56,22 @@ const settingsView = {
         });
 
         // Game save button
-        $("#settings-save").click( function(e) {
+        $("#settings-save").click(function(e) {
             e.preventDefault();
-            $("#settings-saveName").val( settingsController.defaultSaveGameName() );
+            $("#settings-saveName").val(settingsController.defaultSaveGameName());
             $("#settings-saveDialog").modal("show");
         });
 
         // Save game dialog - save button
-        $("#settings-saveBtn").click( function(e) {
+        $("#settings-saveBtn").click(function(e) {
             e.preventDefault();
-            if ( settingsController.saveGame( $("#settings-saveName").val() ) ) {
+            if (settingsController.saveGame($("#settings-saveName").val())) {
                 $("#settings-saveDialog").modal("hide");
             }
         });
 
         // Remove the current file name
-        $("#settings-saveRemove").click( function(e) {
+        $("#settings-saveRemove").click(function(e) {
             e.preventDefault();
             $("#settings-saveName").val("").focus();
         });
@@ -81,7 +81,7 @@ const settingsView = {
      * Show the download book dialog, only for web
      */
     showDownloadDialog() {
-        if ( cordovaApp.isRunningApp() ) {
+        if (cordovaApp.isRunningApp()) {
             return;
         }
         $("#settings-downloadDialog").modal("show");
@@ -91,7 +91,7 @@ const settingsView = {
      * Hide the download book dialog, only for web
      */
     hideDownloadDialog() {
-        if ( cordovaApp.isRunningApp() ) {
+        if (cordovaApp.isRunningApp()) {
             return;
         }
         $("#settings-downloadDialog").modal("hide");
