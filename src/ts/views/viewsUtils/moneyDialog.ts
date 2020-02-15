7ef861e4de3c4979f850d28a0acf62863bf3b389
tonib <toni.bennasar@gmail.com>
2020-02-15 21:16:14 +0100

    Fix src/ts/views/viewsUtils/moneyDialog.ts spaces

--- src/ts/views/viewsUtils/moneyDialog.ts
+++ src/ts/views/viewsUtils/moneyDialog.ts
@@ -9,48 +9,48 @@ class MoneyDialog {
         MoneyDialog.setupDialog(drop);
 
         // Update bounds and initial value
-        if ( drop ) {
+        if (drop) {
             $("#mechanics-moneyamount")
-            .attr("max", state.actionChart.beltPouch )
+            .attr("max", state.actionChart.beltPouch)
             .val("1");
             $("#mechanics-moneyamount").attr("data-ismoneypicker", "true");
         } else {
             const sectionMoney = state.sectionStates.getSectionState().getAvailableMoney();
             $("#mechanics-moneyamount")
-            .attr("max", sectionMoney )
-            .val( sectionMoney );
+            .attr("max", sectionMoney)
+            .val(sectionMoney);
             $("#mechanics-moneyamount").attr("data-ismoneypicker", "false");
         }
 
         const $dlg = $("#mechanics-moneydialog");
 
         // Update translations
-        const title = ( drop ? "dropMoney" : "pickMoney" );
-        $("#mechanics-moneytitle").attr( "data-translation" , title );
-        $("#mechanics-moneyapply").attr( "data-translation" , title );
-        translations.translateTags( $dlg );
+        const title = (drop ? "dropMoney" : "pickMoney");
+        $("#mechanics-moneytitle").attr("data-translation" , title);
+        $("#mechanics-moneyapply").attr("data-translation" , title);
+        translations.translateTags($dlg);
 
         // Show
         $dlg
-            .prop("data-isdrop" , drop )
+            .prop("data-isdrop" , drop)
             .modal("show");
     }
 
     private static setupDialog(drop: boolean) {
 
         // If the dialog HTML do not exists, add it:
-        if ( $("#mechanics-moneydialog").length > 0 ) {
+        if ($("#mechanics-moneydialog").length > 0) {
             return;
         }
 
-        const $moneyDlg = mechanicsEngine.getMechanicsUI( "mechanics-moneydialog" );
-        $("body").append( $moneyDlg );
+        const $moneyDlg = mechanicsEngine.getMechanicsUI("mechanics-moneydialog");
+        $("body").append($moneyDlg);
 
         // Bind money picker events
         $("#mechanics-moneyamount").bindNumberEvents();
 
         // Bind drop money confirmation button
-        $("#mechanics-moneyapply").click( function(e: Event) {
+        $("#mechanics-moneyapply").click(function(e: Event) {
             e.preventDefault();
             MoneyDialog.onDialogConfirmed();
         });
@@ -60,19 +60,19 @@ class MoneyDialog {
     private static onDialogConfirmed() {
 
         const $moneyAmount = $("#mechanics-moneyamount");
-        if ( !$moneyAmount.isValid() ) {
+        if (!$moneyAmount.isValid()) {
             return;
         }
 
         const moneyAmount = $moneyAmount.getNumber();
-        if ( $("#mechanics-moneydialog").prop( "data-isdrop" ) ) {
+        if ($("#mechanics-moneydialog").prop("data-isdrop")) {
             // Drop
-            actionChartController.increaseMoney( - moneyAmount , true );
+            actionChartController.increaseMoney(- moneyAmount , true);
         } else {
             // Pick
-            const countPicked = actionChartController.increaseMoney( moneyAmount );
+            const countPicked = actionChartController.increaseMoney(moneyAmount);
             const sectionState = state.sectionStates.getSectionState();
-            sectionState.removeObjectFromSection( Item.MONEY , 0 , countPicked );
+            sectionState.removeObjectFromSection(Item.MONEY , 0 , countPicked);
             // Re-render section
             mechanicsEngine.showAvailableObjects();
         }
